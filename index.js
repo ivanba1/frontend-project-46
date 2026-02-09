cd72bcd559d52815299df95d67f5b598df2502eb
ivanba1 <astashov.06@gmail.com>
2026-02-09 13:10:40 +0700

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-const genDiff = (filepath1, filepath2) => {
-  console.log(`Comparing ${filepath1} and ${filepath2}...`);
-  return 'Difference will be calculated here!';
-};
-
-export default genDiff;
